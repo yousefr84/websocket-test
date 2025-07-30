f3e75ad5c9bfda767b228c07bf220b796532db52
myous <m.yousef.r84@gmail.com>
2025-07-30 12:03:02 +0330

    solve cors error

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -59,10 +59,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'testsocket'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
